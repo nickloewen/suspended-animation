d650f99d7d41e9be9fe9de23b393a3b279166dc7
Nicholas Loewen <nick@tidepool.ca>
2014-06-30 15:59:52 -0700

    Made the game automatically restart after a certain number of generations, or when a stable pattern is reached. Tidied up code.

--- game.ino
+++ game.ino
@@ -2,8 +2,11 @@
 int cells[8][8];
 // Buffer for state of cells while changing other array
 int cellsBuffer[8][8];
-// count how many generations have elapsed
-int generations = 1;
+
+int generations; // number of generations elapsed since start of game
+int maxGenerations = 100; // maximum number of generations before game is restarted
+
+int serialDebugging = 0; // 0 = off; 1 = on 
 
 void setup() {
   // LED array setup
@@ -13,61 +16,34 @@ void setup() {
   delay(50);
   Init_MAX7219();
   
-  // LIFE setup
-  
-  // initialize cells
-  
-  randomSeed(analogRead(0));
-  
-  for (int x=0; x<8; x++) {
-    for (int y=0; y<8; y++) {
-      float state = random(100);
-      state > 35 ? state = 0 : state = 1; // probability of cell being alive at start
-      cells[x][y] = state;
-    }
-  }
+  // LIFE setup  
+  startGame();
   
   // serial
-  Serial.begin(9600);
- }
+  if (serialDebugging) { Serial.begin(9600); }
+}
  
 void loop() {
   int writeBuffer[8];
   
+  // update game state
   generation();
   
   // check if current generation is different from previous one
+  int repeated = repeatedScreen();
   
-  int allCellsTheSame = 1; // 1 = true, 0 = false  
-  for (int x=0; x<8; x++) {
-    for (int y=0; y<8; y++) {
-      if (cells[x][y] != cellsBuffer[x][y]) {
-        allCellsTheSame = 0;
-        break;
-      }
-    }
-    if (allCellsTheSame == 0) { break; }
+  if ((repeated == 1) || (generations > maxGenerations)) {
+    delay(100);
+    startGame();
   }
   
-  if (allCellsTheSame == 1) {
-    Serial.println("all cells the same");
-    exit(0);
-  }
-  
-  // make binary row values from cells array
-  for (int x=0; x<8; x++) {
-    String acc = "";
-    for (int y=0; y<8; y++) {
-      acc = acc + String(cells[x][y]);
-    }
-    Serial.println(acc);
-    writeBuffer[x] = strtol(acc.c_str(), NULL, 2);
-  }
+  // convert cells array to binary values that LED matrix can read
+  cellsToBinary(writeBuffer);
 
   // update LEDs
-  for (int r=1; r<9; r++) {
-      Write_Max7219(r,writeBuffer[r-1]);
-  }
+  for (int r=1; r<9; r++) { Write_Max7219(r, writeBuffer[r-1]); }
+  
+  // delay between generations
   delay(500);
 }
 
@@ -105,6 +81,62 @@ void generation() {
     }
   }
   generations++;
-  Serial.print("generation: ");
-  Serial.println(generations);
+  if (serialDebugging) {
+    Serial.print("generation: ");
+    Serial.println(generations);
+  }
+}
+
+void startGame() {
+  // setup
+  generations = 1;
+  randomSeed(analogRead(0));
+
+  if (serialDebugging) { Serial.println("NEW GAME"); }
+
+  flashScreen();
+  
+  // initialize each cell
+  for (int x=0; x<8; x++) {
+    for (int y=0; y<8; y++) {
+      float state = random(100);
+      state > 35 ? state = 0 : state = 1; // probability of cell being alive at start
+      cells[x][y] = state;
+    }
+  }
+}
+
+void flashScreen() {
+  int off = B00000000;
+  int on = B11111111;
+ 
+  for (int r=1; r<9; r++) { Write_Max7219(r, off); }
+  delay(50);
+  for (int r=1; r<9; r++) { Write_Max7219(r, on); }
+  delay(50);
+  for (int r=1; r<9; r++) { Write_Max7219(r, off); }
+  delay(50);
+}
+
+int repeatedScreen() {
+  for (int x=0; x<8; x++) {
+    for (int y=0; y<8; y++) {
+      if (cells[x][y] != cellsBuffer[x][y]) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+void cellsToBinary(int writeBuffer[]) {
+  // make binary row values from cells array
+  for (int x=0; x<8; x++) {
+    String acc = "";
+    for (int y=0; y<8; y++) {
+      acc = acc + String(cells[x][y]);
+    }
+    if (serialDebugging) { Serial.println(acc); }
+    writeBuffer[x] = strtol(acc.c_str(), NULL, 2);
+  }
 }
--- todo.md
+++ todo.md
@@ -2,11 +2,12 @@
 
 ## near future
 
-1. check if image is stable
-2. count generations from start to stable
+1. ~~check if image is stable~~
+2. ~~count generations from start to stable~~
 
 ## farther future
 
-1. reset if stable (flash whole screen first?)
-2. reset after a certain # of generations
-3. optimize for running on batteries
+1. ~~reset if stable (flash whole screen first?)~~
+2. ~~reset after a certain # of generations~~
+3. ~~tidy up code~~
+4. optimize for running on batteries
